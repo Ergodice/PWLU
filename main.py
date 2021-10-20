9ee5263979f1c55f325a4f93101de9d74f4c0640
Daniel Monroe <github.com@danielmonroe.net>
2021-10-20 14:02:24 -0700

    Add main and models

---
+++ main.py
@@ -0,0 +1,249 @@
+import torch.jit
+from torch.utils.data import Dataset
+from torchvision import datasets
+import os
+from torchsummary import summary
+import torchvision.transforms as tt
+import tensorflow.summary as tfsummary
+import matplotlib.pyplot as plt
+from models import *
+
+batch_size = 64
+
+DATA_FOLDER = '~/datasets/cifar100'
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Make preprocessing transforms
+stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+trn_tfm = torch.nn.Sequential(tt.RandomCrop(32, padding=[4], padding_mode='reflect'),
+                              tt.RandomHorizontalFlip(),
+                              tt.RandomRotation(10),
+                              tt.Normalize(*stats, inplace=True)
+                              )
+tst_tfm = tt.Normalize(*stats)
+
+trn_tfm = torch.jit.script(trn_tfm)
+tst_tfm = torch.jit.script(tst_tfm)
+
+
+class DataLoader:
+    def __init__(self, ds, n_images: int = 64, shuffle: bool = True):
+        '''
+        :param ds: Dataset
+        :param n_images: int, number of images (default: 64)
+        :param shuffle: bool, whether to shuffle the dataset (default: True)
+
+        '''
+        self.ds = ds
+        self.n_images = n_images
+        self.ds_size = len(self.ds)
+        self.shuffle = shuffle
+
+    def __iter__(self):
+        if self.shuffle:
+            shuffler = np.random.permutation(self.ds_size)
+        position = 0
+        while position < self.ds_size:
+            # n images is size of batch, might be less than self.n_images
+            n_images = min(self.n_images, self.ds_size - position)
+            start, end = position, position + n_images
+            indices = np.arange(start, end)
+            if self.shuffle:
+                indices = shuffler[indices]
+
+            ret = self.ds[indices]
+            if end == len(self.ds):
+                return ret
+            yield ret
+            position += n_images
+
+    def __len__(self):
+        return self.ds_size // self.n_images
+
+
+class CIFARDataset(Dataset):
+    def __init__(self, x, y, n_copies=1, transform=None):
+        '''
+        :param x: numpy array, images
+        :param y: numpy array, labels
+        :param n_copies: int, number of copies of each image (default: 1)
+        :param transform: torchvision.transforms.Compose, transform to apply to images
+        '''
+        self.x = torch.tensor(np.moveaxis(x, -1, 1).astype(np.float32) / 255)
+        self.y = torch.tensor(y)
+        self.transform = transform
+        self.n_copies = n_copies
+
+    def __getitem__(self, idx):
+        x = self.x[idx].to(device)
+        y = self.y[idx].repeat(self.n_copies).to(device)
+        xs = [self.transform(x) for _ in range(self.n_copies)]
+        xs = torch.stack(xs, dim=0).reshape(-1, *xs[0].shape[1:])
+
+        return xs, y
+
+    def __len__(self):
+        return len(self.x)
+
+
+def make_dls():
+    trn_cifar = datasets.CIFAR100(DATA_FOLDER, download=True, train=True)
+
+    trn_images = trn_cifar.data
+    trn_classes = trn_cifar.targets
+    tst_cifar = datasets.CIFAR100(DATA_FOLDER, download=True, train=False)
+    tst_images = tst_cifar.data
+    tst_classes = tst_cifar.targets
+
+    n_copies = 1
+    trn_ds = CIFARDataset(trn_images, trn_classes, n_copies=n_copies, transform=trn_tfm)
+    trn_dl = DataLoader(trn_ds, n_images=batch_size // n_copies, shuffle=True)
+
+    tst_ds = CIFARDataset(tst_images, tst_classes, transform=tst_tfm)
+    tst_dl = DataLoader(tst_ds, n_images=1024)
+    return trn_dl, tst_dl
+
+
+def get_logdir():
+    root_logdir = os.path.join(os.curdir, 'runs')
+    logdir = time.strftime("run_%Y_%m_%d-%H_%M_%S")
+    logdir = os.path.join(root_logdir, logdir)
+    return logdir
+
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def train(epochs, max_lr, model, train_loader, val_loader,
+          weight_decay=0, grad_clip=None, opt_func=torch.optim.SGD, **kwargs):
+    torch.cuda.empty_cache()
+    history = []
+    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay, **kwargs)
+    # optimizer = SAM(model.parameters(), opt_func, max_lr, weight_decay=weight_decay, **kwargs)
+
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs,
+                                                steps_per_epoch=len(train_loader))
+
+    pwlus = [layer for layer in model.modules() if isinstance(layer, PWLU)]
+
+    # sched = torch.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
+
+    for epoch in range(epochs):
+        start = time.perf_counter()
+        # Training Phase
+        model.train()
+        train_losses = []
+        lrs = []
+        batch_num = 0
+
+        for batch in train_loader:
+            if batch_num % 3 == 0:
+                for module in model.modules():
+                    if isinstance(module, PWLU):
+                        module.normalize_points()
+
+            def closure():
+                loss = model.training_step(batch)
+                train_losses.append(loss)
+                loss.backward()
+                if grad_clip:
+                    nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+
+            closure()
+
+            for pwlu in pwlus:
+                if pwlu.points.grad is not None:
+                    pwlu._smooth_gradient(.1)
+
+            # Step
+            optimizer.step()
+            optimizer.zero_grad()
+            batch_num += 1
+            sched.step()
+
+        lrs.append(get_lr(optimizer))
+        train_time = time.perf_counter() - start
+        start = time.perf_counter()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+        valid_time = time.perf_counter() - start
+
+        with writer.as_default():
+            tfsummary.scalar('valid_acc', result['val_acc'], step=epoch)
+            tfsummary.scalar('valid_loss', result['val_loss'], step=epoch)
+        print(f'{train_time=:.2f}s, {valid_time=:.2f}s')
+        if epoch % 5 == 4:
+            torch.save(model.state_dict(), 'newpwlutest' + str(epoch) + '.pth')
+            pass
+
+    '''
+    with torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=5, warmup=1, active=3, repeat=1),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./profiles/00'),
+    ) as prof: 
+    '''
+
+    return history
+
+
+if __name__ == '__main__':
+    model = ResNet().to(device)
+    trn_dl, tst_dl = make_dls()
+    evaluate(model, tst_dl)
+    writer = tfsummary.create_file_writer(get_logdir())
+    summary(model, (3, 32, 32), batch_size=batch_size)
+    epochs = 30
+    max_lr = 0.001
+    grad_clip = 0.1
+    weight_decay = 1e-4
+    optimizer = torch.optim.Adam
+
+    # model.load_state_dict(torch.load('newpwlutest4.pth'))
+    history = [evaluate(model, tst_dl)]
+
+    print(history)
+
+    history += train(epochs, max_lr, model, trn_dl, tst_dl, grad_clip=grad_clip, weight_decay=weight_decay,
+                     opt_func=optimizer)
+
+    print('Finished')
+
+    pwlus = [layer for layer in model.modules() if isinstance(layer, PWLU)]
+
+
+    def plot(i, *j):
+        for index in i:
+            pwlu = pwlus[index]
+            data = pwlu.get_plottable()
+            if j:
+                for jindex in j:
+                    plt.plot(data[0][jindex], data[1][jindex], label=f'{index}.{jindex} {pwlu.init=}')
+            else:
+                plt.plot(data[0], data[1], label=f'{index} {pwlu.init=}')
+        plt.legend()
+        plt.show()
+
+
+    def plot_avg(*i):
+        for index in i:
+            pwlu = pwlus[index]
+            assert pwlu.channelwise
+            data = pwlu.get_plottable()
+            plt.plot(data[0][0], np.average(data[1], axis=0), label=f'{index}.avg {pwlu.init=}')
+        plt.legend()
+        plt.show()
---
+++ models.py
@@ -0,0 +1,212 @@
+from torch.nn.modules.batchnorm import BatchNorm2d
+from pwlu import *
+from utils import *
+
+
+def conv3(in_channels, out_channels=None, activation='relu', norm=BatchNorm2d, bn_zeros=False, pool=False,
+          conv_function=nn.Conv2d, depthwise=False):
+    '''
+    :param in_channels: number of input channels
+    :param out_channels: number of output channels (defaults to in_channels)
+    :param activation: activation function (defaults to relu)
+    :param norm: normalization function (defaults to batchnorm)
+    :param pool: whether to pool (defaults to False)
+    :param conv_function: convolution function (defaults to nn.Conv2d)
+    '''
+    if out_channels is None:
+        out_channels = in_channels
+    activation = get_activation(activation)
+    layers = [conv_function(in_channels, out_channels, kernel_size=3, padding=1, groups=in_channels if depthwise else 1,
+                            bias=not norm)]
+    if norm is not None:
+        if not isinstance(activation, PWLU):
+            layers.append(norm(out_channels))
+            if bn_zeros:
+                assert isinstance(layers[-1], BatchNorm2d)
+                nn.init.constant_(layers[-1].weight, 0)
+    if activation is not None:
+        layers.append(activation)
+    if pool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+def conv1(in_channels, out_channels=None, activation='relu', norm=BatchNorm2d, bn_zeros=False, pool=False,
+          conv_function=nn.Conv2d):
+    '''
+    :param in_channels: number of input channels
+    :param out_channels: number of output channels (defaults to in_channels)
+    :param activation: activation function (defaults to relu)
+    :param norm: normalization function (defaults to batchnorm)
+    :param pool: whether to pool (defaults to False)
+    :param conv_function: convolution function (defaults to nn.Conv2d)
+    '''
+    if out_channels is None:
+        out_channels = in_channels
+    activation = get_activation(activation)
+    layers = [conv_function(in_channels, out_channels, kernel_size=1, bias=not norm)]
+    if norm is not None:
+        layers.append(
+            norm(out_channels, affine=(activation != SquareActivation and not isinstance(activation, PWLU))))
+        if bn_zeros:
+            assert isinstance(layers[-1], BatchNorm2d)
+            nn.init.constant_(layers[-1].weight, 0)
+    if activation is not None:
+        layers.append(activation)
+    if pool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = nn.functional.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = nn.functional.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+class PWLUResBlock(nn.Module):
+    def __init__(self, channels: int):
+        '''
+        :param channels: number of channels in the input and output
+        '''
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.activ1 = PWLU(channels, normed=True)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.activ2 = PWLU(channels, normed=True)
+        self.bn = nn.BatchNorm2d(channels)
+        nn.init.constant_(self.bn.weight, 1)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.activ1(out)
+        out = self.conv2(out)
+        out = self.activ2(out)
+        out = self.bn(out)
+        out = out + x
+        return out
+
+
+class ResBlock(nn.Module):
+
+    def __init__(self, channels: int, use_square: bool = False):
+        '''
+        :param channels: number of channels in the input and output
+        :param use_square: if True, use square activation function
+        '''
+        super().__init__()
+        self.conv1 = conv3(channels, activation='square' if use_square else 'relu')
+        self.conv2 = conv3(channels, activation=None, bn_zeros=True)
+
+        self.bn = nn.BatchNorm2d(channels)
+        self.activation = get_activation('relu')
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = out + x
+        out = self.bn(out)
+        out = self.activation(out)
+        return out
+
+
+class SquareResBlock(nn.Module):
+
+    def __init__(self, channels: int):
+        '''
+        :param channels: number of channels in the input and output
+        :param use_square: if True, use square activation function
+        '''
+        super().__init__()
+        self.conv1 = conv3(channels, channels, activation='square')
+        self.conv2 = conv3(channels, channels, activation=None, bn_zeros=True)
+
+        self.bn = nn.BatchNorm2d(channels)
+        self.activation = get_activation('relu')
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = out + x
+        out = self.bn(out)
+        out = self.activation(out)
+        return out
+
+
+class TestBlock(nn.Module):
+    def __init__(self, channels: int):
+        '''
+        :param channels: number of channels in the input and output
+        :param use_square: if True, use square activation function
+        '''
+        super().__init__()
+        self.conv1 = conv1(channels, channels, activation=PWLU(channels, normed=True, n_regions=6, init='relu'))
+
+        self.bn = nn.BatchNorm2d(channels)
+        self.activation = get_activation('relu')
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = out + x
+        out = self.bn(out)
+        out = self.activation(out)
+        return out
+
+
+class ResNet(ImageClassificationBase):
+    def __init__(self, size=3, in_channels=3, n_classes=100, layer_lengths=[3, 3, 3], use_squares=[False, True, True]):
+        '''
+        :param size: number of blocks per layer
+        :param in_channels: number of input channels
+        :param n_classes: number of output classes
+        '''
+
+        super().__init__()
+
+        self.layers = []
+
+        self.layers.append(conv3(in_channels, 16))
+
+        #self.layers.extend(ResBlock(16, use_squares[0]) for _ in range(layer_lengths[0]))
+
+        self.layers.append(conv3(16, 32, pool=True))
+
+        #self.layers.extend(SquareResBlock(32) for _ in range(layer_lengths[1]))
+
+        self.layers.append(conv3(32, 64, pool=True))
+        self.layers.extend(PWLUResBlock(64) for _ in range(3))
+
+        self.layers.append(BatchNorm2d(64))
+
+        self.layers.append(nn.Flatten())
+        self.layers.append(nn.Linear(64 * 8 * 8, n_classes))
+
+        self.network = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        return self.network(x)
